12b0026f29242ca932ce1046e8b7df79facb0dcb
Mike Burke <mikeburke.214@gmail.com>
2019-10-19 14:49:45 +0100

    Added methods to get and set secret

--- src/ElectraJwt.php
+++ src/ElectraJwt.php
@@ -8,6 +8,8 @@ class ElectraJwt
 {
   /** @var Token | null */
   protected static $token;
+  /** @var string */
+  protected static $secret;
 
   /** @return Token | null */
   public static function getToken(): ?Token
@@ -15,12 +17,22 @@ class ElectraJwt
     return self::$token;
   }
 
-  /**
-   * @param Token $token
-   */
+  /** @param Token $token */
   public static function setToken($token)
   {
     self::$token = $token;
+  }
+
+  /** @return string */
+  public static function getSecret(): string
+  {
+    return self::$secret;
+  }
+
+  /** @param Token $secret */
+  public static function setSecret($secret)
+  {
+    self::$secret = $secret;
   }
 
 }
